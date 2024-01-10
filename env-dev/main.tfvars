4cfbca3ee7d043ae3e40221b19ad9cf8f6e6b70f
surendra Babu C <surendra9703802865@gmail.com>
2024-01-10 19:54:55 +0530

    ansible

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  {name="dev.frontend.catalogue_url", value="http://catalogue-dev.surendrababuc01.online:8080/"},
+  {name="dev.frontend.cart_url", value="http://cart-dev.surendrababuc01.online:8080/"},
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name   = "test.test1"
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name   = var.parameters[count.index].name
   type   = "SecureString"
-  value  = "Hello World"
+  value  = var.parameters[count.index].value
   key_id = "f4c80366-50e1-4dbc-8d1a-102ebb740ae6"
 }

e5f74496de9b609867bc0ffb3f0bfb82f5c7edfd
surendra Babu C <surendra9703802865@gmail.com>
2024-01-10 19:50:49 +0530

    ansible

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
-  name        = "test.test1"
-  type        = "String"
-  value       = "Hello World"
+  name   = "test.test1"
+  type   = "SecureString"
+  value  = "Hello World"
+  key_id = "f4c80366-50e1-4dbc-8d1a-102ebb740ae6"
 }

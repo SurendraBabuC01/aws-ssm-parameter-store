4ecf9f46ef8689bbf2b17618e763c8e77c2eba99
surendra Babu C <surendra9703802865@gmail.com>
2024-01-21 19:01:25 +0530

    ansible

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name   = var.parameters[count.index].name
-  type   = "SecureString"
+  type   = "String"
   value  = var.parameters[count.index].value
+  key_id = "f4c80366-50e1-4dbc-8d1a-102ebb740ae6"
+}
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name   = var.passwords[count.index].name
+  type   = "SecureString"
+  value  = var.passwords[count.index].value
   key_id = "f4c80366-50e1-4dbc-8d1a-102ebb740ae6"
 }
